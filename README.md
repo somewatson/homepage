5d45a1a1dc51adfe2c9d4bca822e57d212ee1498
Some Watson <notjustanywatson@somewatson.com>
2025-07-14 00:55:38 +0000

    First commit
    This is just the initial commit for Some Watson's homepage
    
    Create a place for Some Watson to live on the web like it's 1997. This will be the new page for https://somewatson.com

---
+++ README.md
@@ -0,0 +1,60 @@
+# Some Watson's Home Page
+
+Create a place for Some Watson to live on the web like it's 1997. This will be the new page for https://somewatson.com
+
+### Washodo
+What is Washodo? It's our community we're building for creators. We have an application live and anyone can apply!
+
+**Wa**tson**Sho**DesuPud**do**
+
+### Completed Tasks List
+Monday June 23, 2025
+- [x] Got VS Code up and running with extensions
+- [x] Got the HTML preview server working
+- [x] Learned we could use `sudo`
+- [x] Installed programs like wget, oh-my-zsh, etc.
+- [x] Troubleshooted a regression with code-server
+- [x] Created an index.html file 
+- [x] Imported the Tailwind CSS from CDN
+- [x] Created a css folder, with index.css in it
+
+Tuesday June 24, 2025
+- [x] Applied smooth scrolling
+- [x] Figure out how to add images
+- [x] Made a little Tailwind css card
+- [x] Learned how Tailwind css utility classes work
+
+Wednesday June 25, 2025
+- [x] Added sections for Puddo
+- [x] Autocomplete Tailwind classes in VS Code 
+- [x] Finish out list of things I like
+
+Thursday June 26, 2025
+- [x] Fix grid
+- [x] A floating button at the bottom right that goes back to the top
+- [x] Make the scroll effect lock in at each page, once you start, it'll finish it for you
+
+Saturday June 28, 2025
+- [x] Stream every day this week (wow!)
+- [x] Create a floating table of contents in the top left
+
+Sunday June 29, 2025
+- [x] Indicate which page I'm currently on
+
+### Todo List
+
+Up Next
+- [ ] Create a list of social media links (twitch, youtube, discord)
+
+For Later
+- [ ] Under discord, describe the community we're building (Washodo, the most positive community in League/Gaming)
+- [ ] Core memories and lore
+- [ ] Add a section for friends
+
+### Notes
+* Code-server has 3 methods for accessing web services, such as the HTTP server that runs in preview mode. 
+    * The first is subdomain, the next is subpath, and the final is using your own proxy. 
+    * Source: https://coder.com/docs/code-server/guide#using-a-subpath
+    * We ended up using the subpath method
+* Rano and Sportsliker mentioned, try importing tailwind through the css file for intellisense to work properly
+    * Right now, it's imported inside of the HTML from the CDN
---
+++ js/index.js
@@ -0,0 +1,62 @@
+window.addEventListener('scroll', function () {
+  const div = document.getElementById('up-floating-link');
+  if (window.scrollY == 0) {
+    div.classList.add('opacity-0', 'pointer-events-none');
+    div.classList.remove('opacity-100', 'pointer-events-auto');
+  } else if (window.scrollY > 500) { // Adjust the scroll threshold as needed
+    div.classList.remove('opacity-0', 'pointer-events-none');
+    div.classList.add('opacity-100', 'pointer-events-auto');
+  }
+});
+
+window.addEventListener('load', function() {
+  var isLongScroll = false;
+  document.querySelectorAll('a[class="long-scroll"]').forEach(item =>
+    item.addEventListener('click', function(e) {
+      e.preventDefault(); // Prevent the default smooth scroll behavior
+
+      const targetElement = document.querySelector(this.getAttribute('href'));
+        isLongScroll = true;
+        window.scrollTo({
+          top: targetElement.offsetTop, // Scroll to the element's position
+          behavior: 'smooth'         
+        });
+        setTimeout(() => {
+          isLongScroll = false;
+        }, 500)
+    }
+  ));
+
+  const observer = new IntersectionObserver((entries, observer1) => {
+      for (const entry of entries) {
+          if (entry.isIntersecting) {
+              if (!isLongScroll) {
+                entry.target.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+              }
+              
+              resetToc();
+              const elementId = entry.target.id
+              var item = document.getElementById('toc-'+elementId);
+              item.classList.add("toc-active");
+          }
+      }
+  }, {
+      rootMargin: '0px',
+      threshold: 0.1,
+  });
+
+  observer.observe(document.getElementById('herobox'));
+  observer.observe(document.getElementById('puddobox'));
+  observer.observe(document.getElementById('twoheartsbox'));
+  observer.observe(document.getElementById('communitybox'));
+  observer.observe(document.getElementById('lorebox'));
+})
+
+function resetToc() {
+  const tocs = ['herobox', 'puddobox', 'twoheartsbox', 'communitybox', 'lorebox'];
+  tocs.forEach((elementId) => {
+    var item = document.getElementById('toc-'+elementId);
+    item.classList.remove("toc-active");
+  })
+}
+
